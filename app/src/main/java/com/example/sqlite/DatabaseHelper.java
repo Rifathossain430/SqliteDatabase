4f30dfe94bea674e8785e047d38127f7fc501803
Rifathossain430 <raftone00@gmail.com>
2019-10-09 17:38:23 +0600

    sqlite database create

---
+++ app/src/main/java/com/example/sqlite/DatabaseHelper.java
@@ -0,0 +1,44 @@
+package com.example.sqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static String DATABASE_NAME="User.db";
+    public static String TABLE_NAME="User";
+    public static String COL_ID="Id";
+    public static int VERSION=1;
+    public static String COL_NAME="Name";
+    public static String COL_AGE="Age";
+    public String create_table= "create table "+TABLE_NAME+"(Id integer primary key, Name text,Age text)";
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(create_table);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+    public  long insertData(String Name,String Age){
+        ContentValues contentValues= new ContentValues();
+        contentValues.put(COL_NAME,Name);
+contentValues.put(COL_AGE,Age);
+SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+long id= sqLiteDatabase.insert(TABLE_NAME,null,contentValues);
+sqLiteDatabase.close();
+return id;
+
+
+    }
+}
---
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.sqlite.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private  String name,age;
+    private DatabaseHelper helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        helper = new DatabaseHelper(this);
+
+
+        binding.insertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name= binding.nameET.getText().toString();
+                age= binding.ageET.getText().toString();
+
+                long id= helper.insertData(name,age);
+                Toast.makeText(MainActivity.this, "id is"+id, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
